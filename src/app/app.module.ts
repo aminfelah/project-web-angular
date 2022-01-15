ebb5bce642d2e52cf3486b2d4f7dc1fbd109b4c9
amin felah <aminfelah@gmail.com>
2022-01-15 02:28:03 +0100

    shape of websitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightComponent } from './flight/flight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/flight/flight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight',
+  templateUrl: './flight.component.html',
+  styleUrls: ['./flight.component.css']
+})
+export class FlightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
